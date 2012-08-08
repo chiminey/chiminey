d4109fb8fdbb6697e65a9fe2d6ff3a3d7a064b4e
Ian Thomas <ianedwardthomas@gmail.com>
2012-08-08 16:14:14 +1000

    Added initial notes of steps required

---
+++ bitbucket.py
@@ -0,0 +1,20 @@
+# Deploy Generic Nectar node
+
+# Install required dependencies (e.g., Fortran on VM)
+
+# Transfer program source or binary to node and install
+
+# Get input data from command line or from input files or directories
+
+# Transfer any input files/directores to node using SFTP/SCP
+
+# Run the program on the node.
+
+# Retrieve the data from the node back to the client
+
+# Terminate the instance.
+
+
+# We might want to organise a node to be run multiple times with different parameters, rather than deploying
+# and destroying each time.  This could be an extension if we get time.
+
