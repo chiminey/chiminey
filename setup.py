e7e4cdb32a047daad32ae93d82fc02b6e1801522
Ian Thomas <ianedwardthomas@gmail.com>
2012-08-13 11:30:06 +1000

    Created basic python distribution setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='cloudenabling',
+    version='0.1dev',
+    packages=['cloudenable',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
