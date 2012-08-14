7e8a6c13b9c6ffd7e21c07a80b2a966990713fa6
Ian Thomas <ianedwardthomas@gmail.com>
2012-08-14 16:28:22 +1000

    added sftp put for directories

--- cloudenable/simplepackage.py
+++ cloudenable/simplepackage.py
@@ -88,7 +88,79 @@ def _test_paramiko():
 	ftp = ssh.open_sftp()
 	#ftp.get('remotefile.py', 'localfile.py')
 	#ftp.put('localfile.py','remotefile.py')
+
+	# put whole directory
+	#put_dir_recursively(ssh, localpath, remotepath, preserve_perm=True)
+
 	ssh.close()
+
+
+def _normalize_dirpath(dirpath):
+    while dirpath.endswith("/"):
+        dirpath = dirpath[:-1]
+    return dirpath
+
+
+def _mkdir(sftp, remotepath, mode=0777, intermediate=False):
+    remotepath = .normalize_dirpath(remotepath)
+    if intermediate:
+        try:
+            sftp.mkdir(remotepath, mode=mode)
+        except IOError, e:
+            mkdir(sftp, remotepath.rsplit("/", 1)[0], mode=mode,
+                       intermediate=True)
+            return sftp.mkdir(remotepath, mode=mode)
+    else:
+        sftp.mkdir(remotepath, mode=mode)
+
+
+def _put_dir_recursively(ssh,localpath, remotepath, preserve_perm=True):
+    """
+    	Upload local directory to remote recursively 
+    	from http://stackoverflow.com/questions/4409502/directory-transfers-on-paramiko
+    """
+
+    assert remotepath.startswith("/"), "%s must be absolute path" % remotepath
+
+    # normalize
+    localpath =  _normalize_dirpath(localpath)
+    remotepath = _normalize_dirpath(remotepath)
+
+    sftp = ssh.open_sftp()
+
+    try:
+        sftp.chdir(remotepath)
+        localsuffix = localpath.rsplit("/", 1)[1]
+        remotesuffix = remotepath.rsplit("/", 1)[1]
+        if localsuffix != remotesuffix:
+            remotepath = os.path.join(remotepath, localsuffix)
+    except IOError, e:
+        pass
+
+    for root, dirs, fls in os.walk(localpath):
+        prefix = os.path.commonprefix([localpath, root])
+        suffix = root.split(prefix, 1)[1]
+        if suffix.startswith("/"):
+            suffix = suffix[1:]
+
+        remroot = os.path.join(remotepath, suffix)
+
+        try:
+            sftp.chdir(remroot)
+        except IOError, e:
+            if preserve_perm:
+                mode = os.stat(root).st_mode & 0777
+            else:
+                mode = 0777
+            self._mkdir(sftp, remroot, mode=mode, intermediate=True)
+            sftp.chdir(remroot)
+
+        for f in fls:
+            remfile = os.path.join(remroot, f)
+            localfile = os.path.join(root, f)
+            sftp.put(localfile, remfile)
+            if preserve_perm:
+                sftp.chmod(remfile, os.stat(localfile).st_mode & 0777)
 
 
 if __name__ == '__main__':
