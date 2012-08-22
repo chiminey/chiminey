8d5109158e459f9a0ef47fa91fa7fb51d360a8bf
Ian Thomas <ianedwardthomas@gmail.com>
2012-08-22 17:51:39 +1000

    finished basic setup and run and getting output

--- cloudenable/simplepackage.py
+++ cloudenable/simplepackage.py
@@ -1,60 +1,76 @@
 # Deploy Generic Nectar node
 
-# Install required dependencies (e.g., Fortran on VM)
-# Transfer program source or binary to node and install
-# Get input data from command line or from input files or directories
-# Transfer any input files/directores to node using SFTP/SCP
-# Run the program on the node.
-# Retrieve the data from the node back to the client
-# Terminate the instance.
-# We might want to organise a node to be run multiple times with different parameters, rather than deploying
-# and destroying each time.  This could be an extension if we get time.
-
 import os
 import paramiko
 import settings
+import logging
+import logging
+import logging.config
+logging.config.fileConfig('logging.conf')
 
-
-
+class Error(Exception):
+	pass
+ 
+class PackageFailedError(Error):
+	pass
+ 
 def create_environ():
 	"""
-	Create the Nectar Node and return id
+		Create the Nectar Node and return id
 	"""
 	print "create_environ"
+	# TODO: use libcloud to create a new node and return unique id
 	setup_task(42)
 	return 42
 
 
+def _get_node_ip(instance_id):
+	"""
+		Get the ip adress of a node
+	"""
+	# TODO: use libcloud to return the external ip address from the id
+	return '115.146.94.148'
+
+
+def destroy_environ(instance_id):
+	""" 
+		Terminate the instance 
+	"""
+	print "destroy_environ %s" % instance_id
+	# TODO: use libcloud to tear down the instance
+	pass
+
+
 def setup_task(instance_id):
 	"""
-	Transfer the task package to the node and install
+		Transfer the task package to the node and install
 	"""
 	print "setup_task %s " % instance_id
 	ip = _get_node_ip(instance_id)
-	ssh = _open_connection(ip)
-	_install_deps(ssh, packages=settings.DEPENDS)
+	ssh = _open_connection(ip_address=ip, username=settings.USER_NAME, password=settings.PASSWORD)
+	_install_deps(ssh, packages=settings.DEPENDS,sudo_password=settings.PASSWORD)
 	_mkdir(ssh, dir=settings.DEST_PATH_PREFIX)
 	_put_file(ssh, source_path="payload", package_file=settings.PAYLOAD, environ_dir=settings.DEST_PATH_PREFIX)
 	_unpack(ssh, environ_dir=settings.DEST_PATH_PREFIX, package_file=settings.PAYLOAD)
 	_compile(ssh, environ_dir=settings.DEST_PATH_PREFIX, 
 		compile_file=settings.COMPILE_FILE, 
-		package_dirname=settings.PAYLOAD_DIRNAME)
+		package_dirname=settings.PAYLOAD_DIRNAME,
+		compiler_command=settings.COMPILER)
 
 
 def prepare_input(instance_id, input_dir):
 	"""
-	Take the input_dir and move all the contained files to the node and ready
+		Take the input_dir and move all the contained files to the instance and ready
 	"""
 	print "prepare_input %d %s" % (instance_id, input_dir)
 	ip = _get_node_ip(instance_id)
-	ssh = _open_connection(ip)
+	ssh = _open_connection(ip_address=ip, username=settings.USER_NAME, password=settings.PASSWORD)
 	input_dir = _normalize_dirpath(input_dir)
 	dirList=os.listdir(input_dir)
 	for fname in dirList:
 		print fname
 		_upload_input(ssh, input_dir, fname,
 			os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME))
-
 	_run_command(ssh, "cd %s; cp rmcen.inp rmcen.inp.orig" % 
 		(os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME)))
 	_run_command(ssh, "cd %s; dos2unix rmcen.inp" % 
@@ -63,43 +79,61 @@ def prepare_input(instance_id, input_dir):
 		(os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME)))
 
 
-
 def run_task(instance_id):
 	"""
-	Start the task on the node
+		Start the task on the instance, then hang and periodically check its state.
 	"""
 	print "run_task %s" % instance_id 
 	ip = _get_node_ip(instance_id) 
-	ssh = _open_connection(ip)
-	_run_command(ssh, "cd %s; ./a.out >& output &" % (os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME)))
-
+	ssh = _open_connection(ip_address=ip, username=settings.USER_NAME, password=settings.PASSWORD)
+	if len(_get_package_pid(ssh,settings.COMPILE_FILE)) > 1:
+		print "warning:multiple packages running"
+	_run_command(ssh, "cd %s; ./%s >& %s &" % (os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME),
+		settings.COMPILE_FILE, "output"))
+	import time
+	attempts = settings.RETRY_ATTEMPTS
+	for x in range(0, attempts):
+		time.sleep(5) # to give process enough time to start
+		pid = _get_package_pid(ssh,settings.COMPILE_FILE)
+		print pid
+		if pid:
+			break
+	else:
+		raise PackageFailedError("package did not start") 
+	return pid.split(' ')
 	pass
 
 
-def get_output(instance_id):
+def get_output(instance_id,output_dir):
 	""" 
-	Retrieve the output from the task on the node
+		Retrieve the output from the task on the node
 	"""
 	print "get_output %s" % instance_id
+	ip = _get_node_ip(instance_id) 
+	ssh = _open_connection(ip_address=ip, username=settings.USER_NAME, password=settings.PASSWORD)
+	try:
+		os.mkdir(output_dir)
+	except exception.OSError:
+		logging.warning("output directory already exists")
+		sys.exit(1)	
+	logging.info("output directory is %s" % output_dir)	
+	for file in settings.OUTPUT_FILES:
+		_get_file(ssh, os.path.join(settings.DEST_PATH_PREFIX, settings.PAYLOAD_DIRNAME), file, output_dir)
+	# TODO: do integrity check on output files
 	pass
 
-def destroy_environ(instance_id):
+
+def job_finished(instance_id):
 	""" 
-	terminate the instance 
+		Return True if package job on instance_id has job_finished
 	"""
-	print "destroy_environ %s" % instance_id
-	pass
+	ip = _get_node_ip(instance_id) 
+	ssh = _open_connection(ip_address=ip, username=settings.USER_NAME, password=settings.PASSWORD)
+	pid = _get_package_pid(ssh,settings.COMPILE_FILE) 
+	return not pid
 
 
-def _get_node_ip(instance_id):
-	"""
-		Get the ip adress of a node
-	"""
-	# TODO: use libcloud to return the external ip address of the node
-	return '115.146.94.148'
-
-
-def _open_connection(ip_address):
+def _open_connection(ip_address, username, password):
 	# open up the connection
 	ssh = paramiko.SSHClient()
 	paramiko.util.log_to_file("out")
@@ -107,10 +141,10 @@ def _open_connection(ip_address):
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
 
-	# TODO: use PKI
-	print "%s %s %s" % (ip_address, settings.USER_NAME, settings.PASSWORD)
+	# TODO: use PKI rather than username/password
+	print "%s %s %s" % (ip_address, username, password)
 	print ssh
-	ssh.connect(ip_address, username=settings.USER_NAME, password=settings.PASSWORD, timeout=60)
+	ssh.connect(ip_address, username=username, password=password, timeout=60)
 	return ssh
 
 
@@ -129,6 +163,38 @@ def _run_sudo_command(ssh,command,password=None):
 	return stdout.readlines()
 
 
+def _unpack(ssh,environ_dir,package_file):
+	print _run_command(ssh,'tar --directory=%s --extract --gunzip --verbose --file=%s' 
+		% (environ_dir, os.path.join(environ_dir, package_file)))
+
+
+def _compile(ssh, environ_dir,compile_file, package_dirname,compiler_command):
+	_run_command(ssh, "%s %s.f -o %s " % (compiler_command, compile_file, compile_file),
+		current_dir=os.path.join(environ_dir, package_dirname))
+
+
+def _install_deps(ssh,packages,sudo_password):
+	for pack in packages:
+	    _run_sudo_command(ssh,'sudo yum install %s' % pack,sudo_password)
+
+
+def _upload_input(ssh,source_path_prefix,input_file,dest_path_prefix):
+		_put_file(ssh, source_path_prefix, input_file,dest_path_prefix)		
+
+
+def _mkdir(ssh,dir):
+	_run_command(ssh,"mkdir %s" % dir)
+
+
+def _get_file(ssh, source_path, package_file, environ_dir):
+	ftp = ssh.open_sftp()
+	print source_path, package_file, environ_dir
+	source_file = os.path.join(source_path,package_file).replace('\\','/')
+	dest_file = os.path.join(environ_dir,package_file).replace('\\', '/')
+	print source_file, dest_file
+ 	ftp.get(source_file, dest_file)
+
+
 def _put_file(ssh, source_path, package_file, environ_dir):
 	ftp = ssh.open_sftp()
 	print source_path, environ_dir
@@ -138,29 +204,16 @@ def _put_file(ssh, source_path, package_file, environ_dir):
  	ftp.put(source_file, dest_file)
 
 
-def _unpack(ssh,environ_dir,package_file):
-	print _run_command(ssh,'tar --directory=%s --extract --gunzip --verbose --file=%s' 
-		% (environ_dir, os.path.join(environ_dir, package_file)))
-
-
-def _compile(ssh, environ_dir,compile_file, package_dirname):
-	_run_command(ssh, "%s %s" % (settings.COMPILER, compile_file),
-		current_dir=os.path.join(environ_dir, package_dirname))
-
-def _install_deps(ssh,packages):
-	for pack in packages:
-	    _run_sudo_command(ssh,'sudo yum install %s' % pack,settings.PASSWORD)
-
-
-def _upload_input(ssh,source_path_prefix,input_file,dest_path_prefix):
-		_put_file(ssh, source_path_prefix, input_file,dest_path_prefix)		
-
-def _mkdir(ssh,dir):
-	_run_command(ssh,"mkdir %s" % dir)
-
-
 def _normalize_dirpath(dirpath):
     while dirpath.endswith("/"):
         dirpath = dirpath[:-1]
     return dirpath
+
+
+def _get_package_pid(ssh,command):
+	pid = _run_command(ssh,"/sbin/pidof %s" % command)
+	if len(pid):
+		pid = pid[0] # if some returns, the pids are in first element
+	return pid
+
 
--- mc.py
+++ mc.py
@@ -1,5 +1,11 @@
 from cloudenable.simplepackage import *
 from optparse import OptionParser
+import settings
+import sys		
+import time
+import logging
+
+
 
 if __name__ == '__main__':
 
@@ -15,24 +21,70 @@ if __name__ == '__main__':
 	parser.add_option("-n", "--nodeid", type="int", dest="instance_id",
 	                  help="The instance id from the cloud infrastructure")
 	parser.add_option("-i", "--inputdir", dest="input_dir",
-	                  help="The local directory holding input files for the task")	
+	                  help="The local directory holding input files for the task")		
+	parser.add_option("-o", "--outputdir", dest="output_dir",
+	                  help="The local directory which will hold output files for the task")	
+
 	(options, args) = parser.parse_args()
 
 	if 'setup' in args:
 		print create_environ()	
 	elif 'run' in args:
 		if options.instance_id:		
+			if not options.output_dir:
+				logging.error("need to specify output directory")
+				parser.print_help()
+				sys.exit(1)
+			elif os.path.isdir(options.output_dir):
+				logging.error("output directory already exists")
+				sys.exit(1)				
 			id = options.instance_id
 			prepare_input(id,options.input_dir)
-			run_task(id)
-			get_output(id)
+			job_id = run_task(id)	
+			print job_id	
+			if (len(job_id) != 1):
+				logging.warn("warning: muliple payloads running")
+			while (True):
+				if job_finished(id):
+					break
+				logging.info("job is running.  Wait or CTRL-C to exit here.  run 'check' command to poll again")
+				time.sleep(settings.SLEEP_TIME)
+			if options.output_dir:			
+				get_output(id,options.output_dir)
+			else:
+				logging.error("need to specify output directory")
+				parser.print_help()
+				sys.exit(1)
 		else:
+			logging.error("enter nodeid of the package")
 			parser.print_help()
+			sys.exit(1)
+
+	elif 'check' in args:
+		if options.instance_id:
+			if not options.output_dir:
+				logging.error("need to specify output directory")
+				parser.print_help()
+				sys.exit(1)
+			elif os.path.isdir(options.output_dir):			
+					logging.error("output directory already exists")
+					sys.exit(1)	
+			id = options.instance_id
+			if job_finished(options.instance_id):
+				print "done"	
+				get_output(id,options.output_dir)
+		else:
+			print "enter nodeid of the package"		
+			parser.print_help() 
+			sys.exit(1)
 	elif 'teardown' in args:
 		if options.instance_id:
 			id = options.instance_id
 			destroy_environ(id)
 		else:
+			print "enter nodeid of the package"
 			parser.print_help()
+			sys.exit(1)
 	else:
 		parser.print_help()
+
