68fb15412c6a3b5244abebcf6dc3156f3c6da40e
Iman Yusuf <iimanyusuf@gmail.com>
2012-08-23 16:36:32 +1000

    bug fixed: settings.py added

---
+++ settings.py
@@ -0,0 +1,13 @@
+USER_NAME = "username" #account name of instance 
+PASSWORD = "password"  #account password of instance
+PAYLOAD = "aeao_v1_1_package.tar.gz" # the filename of the package
+DEST_PATH_PREFIX="package" # The destination directory for the software to run 
+DEPENDS = ['dos2unix','gcc-gfortran','compat-gcc-34-g77.x86_64'] # yum install requirements
+COMPILER="g77" # The compiler command
+COMPILE_FILE="HRMC" # The file to be compiled (no extension)
+PAYLOAD_DIRNAME="AEAO_v1_1" #The directory of the unpackaed payload
+
+SLEEP_TIME = 10 # time to wait before polling finished job in seconds
+RETRY_ATTEMPTS = 10 # number of times to try accessing package PID
+
+OUTPUT_FILES = ['output','energy01.dat','spchn01.dat']
