21bd94363362754ab258b8258ad6dd76aca48218
Iman Yusuf <iimanyusuf@gmail.com>
2013-03-15 12:05:48 +1100

    Create stage updated to use new run and user settings

--- bdphpcprovider/smartconnectorscheduler/stages/create.py
+++ bdphpcprovider/smartconnectorscheduler/stages/create.py
@@ -13,51 +13,52 @@ logger = logging.getLogger(__name__)
 
 class Create(Stage):
 
-    def __init__(self):
-        self.settings = {}
+    def __init__(self, user_settings=None):
+        self.user_settings = user_settings
+        self.settings = dict(self.user_settings)
         self.group_id = ''
-        self.provider = None
+        self.platform = None
 
-    def triggered(self, context):
+    def triggered(self, run_settings):
         """
-            Return True if there is a file system and a filesystem and there is a provider
-            but now group_id
+            Return True if there is a platform
+            but not group_id
         """
-        self.settings = get_settings(context)
-        self.run_info = get_run_info(context)
+        #self.settings = get_settings(run_settings)
+        #self.run_info = get_run_info(run_settings)
 
-        if self.settings and self.run_info:
-            if 'PROVIDER' in self.run_info:
-                self.provider = self.run_info['PROVIDER']
-                if 'group_id' in self.run_info:
-                    return False
-                self.settings.update(self.run_info)  # merge all settings
-
-                return True
+        logger.debug("User_settings %s \n Run_settings %s" % (self.user_settings, run_settings))
+        if (not run_settings[u'group_id']) and run_settings[u'flag'] == 0 :
+            if u'platform' in run_settings:
+                self.platform = run_settings[u'platform']
+            self.settings.update(run_settings)
+            logger.debug("Settings: %s" % self.settings)
+            logger.debug("Create Stage: Triggered")
+            run_settings[u'flag'] = 1
+            return True
+        logger.debug("Create Stage: Not Triggered")
         return False
 
-    def process(self, context):
+    def process(self, run_settings):
         """
         Make new VMS and store group_id
         """
-        number_vm_instances = context['number_vm_instances']
-        self.seed = context['seed']
+        self.settings.update(run_settings)
+        number_vm_instances = self.settings[u'number_vm_instances']
+        logger.debug("VM instance %d" % number_vm_instances)
         self.group_id = create_environ(number_vm_instances, self.settings)
         if not self.group_id:
-            print "No new VM instance can be created for this computation. Retry later."
-            clear_temp_files(context)
-            sys.exit()
+            self.group_id = ''
+            logger.debug("No new VM instance can be created for this computation. Retry later.")
+            #clear_temp_files(run_settings)
+            #sys.exit()
 
-    def output(self, context):
+    def output(self, run_settings):
         """
-        Create a runfinos.sys file in filesystem with new group_id
+        Inserting a new group if into run settings.
         """
-        local_filesystem = 'default'
-        data_object = DataObject("runinfo.sys")
-        data_object.create(json.dumps({'group_id': self.group_id,
-                                       'seed': self.seed,
-                                       'PROVIDER': self.provider}))
-        filesystem = get_filesys(context)
-        filesystem.create(local_filesystem, data_object)
-        return context
+
+        run_settings[u'group_id'] = self.group_id
+        logger.debug("Updated run settings %s" % run_settings)
+        return run_settings
 
