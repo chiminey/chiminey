5b358fcf82c6e591d425697b5de7e7da40082bc0
Iman Yusuf <iimanyusuf@gmail.com>
2013-08-19 16:32:10 +1000

    reliability framework being developed

---
+++ bdphpcprovider/reliabilityframework/failuredetection.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2013, RMIT University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import logging
+
+from bdphpcprovider.reliabilityframework.ftmanager import FTManager
+
+
+logger = logging.getLogger(__name__)
+
+
+class FailureDetection(FTManager):
+    def sufficient_vms(self, created_vms, min_number_vms):
+        if int(created_vms) < int(min_number_vms):
+            logger.error('Insufficient no. VMs created')
+            logger.info('created_nodes=%d but minimmum_requirement=%d'
+                        % (created_vms, min_number_vms))
+            return False
+        return True
+
+
+
