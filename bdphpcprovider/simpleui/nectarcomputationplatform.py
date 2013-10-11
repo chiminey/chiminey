06e1983f4edcae05dec18ec4f3233e2ac6563962
Iman Yusuf <iimanyusuf@gmail.com>
2013-10-12 07:22:19 +1100

    refactoring

--- bdphpcprovider/simpleui/nectarcomputationplatform.py
+++ bdphpcprovider/simpleui/nectarcomputationplatform.py
@@ -6,9 +6,3 @@ class NeCTARComputationPlatformForm(forms.Form):
     nectar_username = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
     private_key_path = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
     operation = forms.CharField(initial='update', widget=forms.HiddenInput(attrs={'required': 'false'}))
-
-    #def __init__(self, *args, **kwargs):
-    #    super(NeCTARComputationPlatformForm, self).__init__(*args, **kwargs)
-        #self.fields["username"].validators.append(validators.validate_not_null)
-        #self.fields["run_map"].validators.append(validators.validate_run_map)
-        #self.fields["private_key_path"].validators.append(validators.validate_not_null)
