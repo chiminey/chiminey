f39b54ca26bc49a7c51966c294430f9f4ef9fb5f
Iman Yusuf <iimanyusuf@gmail.com>
2013-10-13 16:40:00 +1100

    platform GUI for update completed

--- bdphpcprovider/simpleui/ncicomputationplatform.py
+++ bdphpcprovider/simpleui/ncicomputationplatform.py
@@ -6,3 +6,4 @@ class NCIComputationPlatformForm(forms.Form):
     username = forms.CharField(initial="", required=True, widget=forms.TextInput(attrs={'required': 'true'}))
     private_key_path = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
     operation = forms.CharField(initial='update', widget=forms.HiddenInput(attrs={'required': 'false'}))
+    filters = forms.CharField(initial='{}', widget=forms.HiddenInput(attrs={'required': 'false'}))
