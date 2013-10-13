296a5fba0f1955b77d9b7b8db333462843e07aec
Iman Yusuf <iimanyusuf@gmail.com>
2013-10-13 20:45:00 +1100

    refactoring

--- bdphpcprovider/simpleui/nectarcomputationplatform.py
+++ bdphpcprovider/simpleui/nectarcomputationplatform.py
@@ -3,6 +3,11 @@ from bdphpcprovider.simpleui import validators
 
 
 class NeCTARComputationPlatformForm(forms.Form):
-    nectar_username = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
-    private_key_path = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
+    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
+    ec2_secret_key = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
+    ec2_access_key = forms.CharField(required=True, widget=forms.TextInput(attrs={'required': 'true'}))
+    private_key_path = forms.CharField(initial='/to/be/generated.pem', widget=forms.HiddenInput(attrs={'required': 'false'}))
+    private_key = forms.CharField(initial='generated', widget=forms.HiddenInput(attrs={'required': 'false'}))
+    vm_image_size = forms.CharField(initial='m1.small', required=False)
     operation = forms.CharField(initial='update', widget=forms.HiddenInput(attrs={'required': 'false'}))
+    filters = forms.CharField(initial='{}', widget=forms.HiddenInput(attrs={'required': 'false'}))
