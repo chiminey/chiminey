e9926b6583e307898f378e20cfd155ee1d073b80
Ian Thomas <ianedwardthomas@gmail.com>
2014-03-05 13:39:23 +1100

    added doc chiminey path

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./chiminey'))
 
 from chiminey import settings
 from django.core.management import setup_environ
