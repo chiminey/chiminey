15652e216545246de8534b6ce5329ba0fba5d45d
Ian Thomas <ianedwardthomas@gmail.com>
2014-03-05 13:31:43 +1100

    fixed path

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
 from chiminey import settings
 from django.core.management import setup_environ
 setup_environ(settings)
@@ -19,7 +22,6 @@ setup_environ(settings)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
 
