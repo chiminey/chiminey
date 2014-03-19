e0786d5859b996fe845c3d47145be977e4b613d1
Ian Thomas <ianedwardthomas@gmail.com>
2014-03-19 14:52:33 +1100

    added back mock

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 
 
 import mock
-MOCK_MODULES = ['psycopg2']
+MOCK_MODULES = ['psycopg2', 'psycopg2.extensions']
 for mod_name in MOCK_MODULES:
 	sys.modules[mod_name] = mock.Mock()
 sys.path.insert(0, os.path.abspath('..'))
