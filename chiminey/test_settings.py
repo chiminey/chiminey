b1725eb462e176c0a2d234e34375cd0ea94d3789
Ian Thomas <ianedwardthomas@gmail.com>
2014-03-21 12:13:12 +1100

    added (failing) testcases

--- chiminey/test_settings.py
+++ chiminey/test_settings.py
@@ -1,8 +1,18 @@
 from chiminey.settings_changeme import *
 
+DEBUG=True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': ':memory:',
     }
 }
+
+ROOT_URLCONF = "chiminey.urls"
+
+TEMPLATE_DIRS = ["."]
+INSTALLED_APPS += ('django_nose', )
+
+NOSE_ARGS = ['--verbosity=3']
+
+
