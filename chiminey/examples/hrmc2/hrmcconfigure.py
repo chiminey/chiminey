04c145a3540c000021042a66df7c428f234181f3
Ian Thomas <ianedwardthomas@gmail.com>
2014-06-19 11:08:00 +1000

    added hrmc input metadata to mytardis experiment

--- chiminey/examples/hrmc2/hrmcconfigure.py
+++ chiminey/examples/hrmc2/hrmcconfigure.py
@@ -5,6 +5,8 @@ from chiminey.corestages import Configure
 
 from chiminey.platform import manage
 from chiminey import mytardis
+from chiminey.runsettings import getval, SettingNotFoundException
+
 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +29,25 @@ class HRMCConfigure(Configure):
             def _get_exp_name_for_input(path):
                 return str(os.sep.join(path.split(os.sep)[-EXP_DATASET_NAME_SPLIT:]))
 
+            hrmcexp = []
+            for (sch, n) in (('hrmc', 'iseed'),
+                      ('hrmc', 'pottype'),
+                      ('hrmc', 'error_threshold'),
+                      ('hrmc', 'optimisation_scheme'),
+                      ('hrmc', 'fanout_per_kept_result'),
+                      ('hrmc', 'threshold'),
+                      ('hrmc', 'max_iteration'),
+                      ('sweep', 'sweep_map'),
+                      ('system', 'input_location'),
+                      ('system', 'output_location'),
+                      ('system/compplatform/cloud', 'computation_platform')
+                       ):
+                try:
+                    hrmcexp.append({'name': n, 'value': getval(run_settings,
+                                '%s/input/%s/%s' % (RMIT_SCHEMA, sch, n))})
+                except SettingNotFoundException:
+                    logger.warn('%s/input/%s/%s value not found' % (RMIT_SCHEMA, sch, n))
+
             logger.debug("location=%s" % location)
             ename = _get_exp_name_for_input(location)
             logger.debug("ename=%s" % ename)
@@ -35,7 +56,7 @@ class HRMCConfigure(Configure):
                 exp_id=experiment_id,
                 expname=ename,
                 experiment_paramset=[
-                    mytardis.create_paramset("hrmcexp", []),
+                    mytardis.create_paramset("hrmcexp", hrmcexp),
                     mytardis.create_graph_paramset("expgraph",
                         name="hrmcexp",
                         graph_info={"axes":["iteration", "criterion"], "legends":["criterion"], "precision":[0, 2]},
