b259d5dfc8b58a6ffe11fc0c0c51750a872876df
root <abdullah.cs@gmail.com>
2018-02-01 01:34:00 +0000

    took off myTardis support in the chiminey portal

--- chiminey/prismconnector/initialise.py
+++ chiminey/prismconnector/initialise.py
@@ -37,7 +37,7 @@ class PrismInitial(CoreInitial):
             u'http://rmit.edu.au/schemas/system':
                 {
                     u'random_numbers': 'file://127.0.0.1/randomnums.txt',
-                    u'metadata_builder': 'chiminey.mytardis.metadata.MetadataBuilder',
+                    #u'metadata_builder': 'chiminey.mytardis.metadata.MetadataBuilder',
                 },
         }
         return { 'settings': settings}
@@ -50,7 +50,7 @@ class PrismInitial(CoreInitial):
                 django_settings.INPUT_FIELDS['input_location'],
                 django_settings.INPUT_FIELDS['output_location'],
                 django_settings.INPUT_FIELDS['prism'],
-                django_settings.INPUT_FIELDS['mytardis'],
+                #django_settings.INPUT_FIELDS['mytardis'],
                 ]
         return schemas
 
