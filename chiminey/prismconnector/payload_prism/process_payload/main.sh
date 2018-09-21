8d895b11857624984b9f162bd4739c922a6e592b
Ahmed Abdullah <abdullah.cs@gmail.com>
2018-09-21 06:29:29 +0000

    inject bencmarking data from master brance if timdata.txt file is available

--- chiminey/prismconnector/payload_prism/process_payload/main.sh
+++ chiminey/prismconnector/payload_prism/process_payload/main.sh
@@ -4,15 +4,16 @@ INPUT_DIR=$1
 OUTPUT_DIR=$2
 
 cp run.sh $INPUT_DIR/run.sh
-
 RUN_DIR=`cd "$(dirname "$0")" && pwd`
 
-echo $RUN_DIR > mainsh.output
+exec_start_time=`date +"%Y-%m-%d %H:%M:%S.%3N"`
+sed -i "s/EXEC_START_TIME/$exec_start_time/" ./timedata.txt
 
 sh $RUN_DIR/run.sh $@
 
 exec_end_time=`date +"%Y-%m-%d %H:%M:%S.%3N"`
 sed -i "s/EXEC_END_TIME/$exec_end_time/" ./timedata.txt
 cp ./timedata.txt $OUTPUT_DIR/timedata.txt
+
 
 # --- EOF ---
