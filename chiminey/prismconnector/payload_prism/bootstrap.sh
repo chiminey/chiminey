f1619d2c7b313a77e2154ffb9c27dc856e53a26f
Ahmed Abdullah <abdullah.cs@gmail.com>
2018-10-03 05:51:14 +0000

    adapted to new oracle java download address

--- chiminey/prismconnector/payload_prism/bootstrap.sh
+++ chiminey/prismconnector/payload_prism/bootstrap.sh
@@ -5,18 +5,24 @@
 WORK_DIR=`pwd`
 
 # Install Java
-ext="tar.gz"
-jdk_version=8
+jdk_version=${1:-8}
+ext=tar.gz
 
-# how to get the latest oracle java version ref: https://gist.github.com/n0ts/40dd9bd45578556f93e7
-cd /opt/
-readonly url="http://www.oracle.com"
+readonly url="https://www.oracle.com"
 readonly jdk_download_url1="$url/technetwork/java/javase/downloads/index.html"
-readonly jdk_download_url2=$(curl -s $jdk_download_url1 | egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${jdk_version}-downloads-.+?\.html" | head -1 | cut -d '"' -f 1)
-[[ -z "$jdk_download_url2" ]] && error "Could not get jdk download url - $jdk_download_url1"
+readonly jdk_download_url2=$(
+    curl -s $jdk_download_url1 | \
+    egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${jdk_version}-downloads-.+?\.html" | \
+    head -1 | \
+    cut -d '"' -f 1
+)
+[[ -z "$jdk_download_url2" ]] && echo "Could not get jdk download url - $jdk_download_url1" >> /dev/stderr
 
 readonly jdk_download_url3="${url}${jdk_download_url2}"
-readonly jdk_download_url4=$(curl -s $jdk_download_url3 | egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[7-8]u[0-9]+\-(.*)+\/jdk-[7-8]u[0-9]+(.*)linux-x64.$ext")
+readonly jdk_download_url4=$(
+    curl -s $jdk_download_url3 | \
+    egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/jdk-${jdk_version}.*(-|_)linux-(x64|x64_bin).$ext"
+)
 
 for dl_url in ${jdk_download_url4[@]}; do
     wget --no-cookies \
